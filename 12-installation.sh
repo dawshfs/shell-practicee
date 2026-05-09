13813a0c1db09bb22a34fa6463148dd2d4670f4e
ajay <ajay.wlsdyn@gmail.com>
2026-05-09 16:13:33 +0530

    hi

--- 12-installation.sh
+++ 12-installation.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root user"
+    exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR: :Installing Mysql is failure"
+    exit 1
 else
     echo " Installing mysql is SUCCESS"
 fi
