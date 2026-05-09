864b845bfcce89397608e2f694ed95506647c812
ajay <ajay.wlsdyn@gmail.com>
2026-05-09 15:35:32 +0530

    session-16

---
+++ 10-conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NUMBER=$1  
+
+if [ $NUMBER -lt 10]; than
+    echo "Given number is $NUMBER is less than 10"
+else
+    echo " Given number is $NUMBER is greater than 10"
+fi
+
+# -gt
+# -eq
+# -lt
